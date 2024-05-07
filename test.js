d200f170391e729c20c8c8e4861112eec180fd7a
Prajesh Mishrikotkar <prajeshm07@gmail.com>
2024-05-07 09:57:21 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
+import react from 'react';
+import { FixedSizeList as List } from 'react-window';
 
+const MyList = ({ items })=> (
+  <List 
+    height={500}
+    itemCount={items.length}
+    itemSize={35}
+    width={300}>
+      {({ index, style }) => (
+        <div style={style}>
+          {items [index].name}
+        </div>
+      )}
+  </List>
+);
+
+export default MyList;
